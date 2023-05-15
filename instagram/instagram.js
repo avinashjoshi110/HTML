fa499e169c2e1054a0ef263f1542f7e66c8b6952
unknown <joshiavinash2000@gmail.com>
2023-05-15 13:09:01 +0530

    instagram

---
+++ instagram/instagram.js
@@ -0,0 +1,15 @@
+let ha = document.querySelector('h1')
+ha.textContent="Meta Is Instagram"
+ha.style.fontSize="35px"
+ha.style.color="teal"
+
+let hb = document.getElementById('case1')
+hb.textContent="Mark Zukerburg's place"
+hb.style.fontSize="25px"
+hb.style.color="Brown"
+
+let hc = document.getElementById('case2')
+hc.textContent="IG by Meta"
+hc.style.fontSize="30px"
+
+let hd = document.getElementsByClassName('contact')

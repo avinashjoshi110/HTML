cdb31cd0a21f917e5e7c5fe125a8d1369fdd40d4
unknown <joshiavinash2000@gmail.com>
2023-05-15 13:04:08 +0530

    form

---
+++ form/form1.js
@@ -0,0 +1,6 @@
+function ValidateForm() {
+    const name = document.getElementById('name').value;
+    const pass = document.getElementById('password').value;
+    document.write(`your name is ${name}`);
+    document.write(`Your password is ${pass}`);
+}
